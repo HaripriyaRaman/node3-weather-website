ad17901b72b5880a2dbbac8d5cbfe7a66c86a8de
unknown <haripriyaraman98@gmail.com>
2020-09-09 13:13:41 +0530

    added entities to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,11 @@
 const request = require('postman-request')
 const forecast = (latitude,longitude,callback) =>{
 const url = 'http://api.weatherstack.com/current?access_key=79928d79cae90bc11346c2a9d1ada0ce&query='+ longitude +',' + latitude + '&units=f'
+
+
+
+
+
 //request({url: url,json: true},(error,response)=>{
 request({url,json: true},(error,response)=>{                            /* Usage of shorthand syntax */
     //console.log(response.body.current)
@@ -10,7 +15,7 @@ request({url,json: true},(error,response)=>{                            /* Usage
         callback("Unable to find the location",undefined)
     }   
     else{
-        callback(undefined,"It is currently " + response.body.current.temperature + " degrees out. There is a " + response.body.current.precip + "% chance of rain. Weather Description:" + response.body.current.weather_descriptions[0])
+        callback(undefined,"It is currently " + response.body.current.temperature + " degrees out. There is a " + response.body.current.precip + "% chance of rain. Weather Description:" + response.body.current.weather_descriptions[0] + "Humidity is " +response.body.current.humidity + 'and the pressure is '+ response.body.current.pressure )
          //callback(undefined,'Unable to connect to location services')
     } 
     
